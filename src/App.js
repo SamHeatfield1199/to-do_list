a79948dc340314fd1041243ffa196e556b77b9ea
SamHeatfield1199 <asmirnova1199@gmail.com>
2022-01-12 15:46:58 +1000

    finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import randomColor from "randomcolor";
+import { useEffect, useState } from "react";
+import Draggable from "react-draggable";
+import { v4 } from "uuid";
+import "./App.css";
 
 function App() {
+  const [item, setItem] = useState("");
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("items")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
+
+  const newItem = () => {
+    if (item.trim() !== "") {
+      const newItem = {
+        id: v4(),
+        item: item,
+        color: randomColor({
+          luminosity: "light",
+        }),
+        defaultPosition: {
+          x: 500,
+          y: -500,
+        },
+      };
+      setItems((item) => [...items, newItem]);
+      setItem("");
+    } else {
+      alert("Enter something >:(");
+      setItem("");
+    }
+  };
+
+  const deleteItem = (id) => {
+    setItems(items.filter((item) => item.id !== id));
+  };
+  const updatePosition = (data, index) =>{
+    let newArray = [...items]
+    newArray[index].defaultPosition = {
+      x: data.x,
+      y: data.y,
+    }
+    setItems(newArray)
+  };
+
+  const keyPress = (e) =>{
+      const code = e.keyCode || e.which 
+      if (code === 13){
+        newItem()
+      }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <input
+          value={item}
+          className="input"
+          type="text"
+          placeholder="input your important tasks"
+          onChange={(e) => setItem(e.target.value)}
+          onKeyPress={(e) => keyPress(e)}
+        />
+        <button className="enter" onClick={newItem}>
+          Enter
+        </button>
+      </div>
+      {items.map((item, index) => {
+        return (
+          <Draggable
+            onStop={(_, data) => {
+              updatePosition(data, index);
+            }}
+            key={index}
+            defaultPosition={item.defaultPosition}
+          >
+            <div className="item" style={{ backgroundColor: `${item.color}` }}>
+              {`${item.item}`}
+              <button className="delete" onClick={() => deleteItem(item.id)}>
+                x
+              </button>
+            </div>
+          </Draggable>
+        );
+      })}
     </div>
   );
 }
